28ca95a1a005c215c5fa9b8b62070bc9bdee39c5
braykoff <99614905+Braykoff@users.noreply.github.com>
2023-02-03 21:48:20 -0500

    Fixed unneeded dep

--- src/main/java/com/frc7153/SwerveDrive/WheelTypes/SwerveWheel_FN.java
+++ src/main/java/com/frc7153/SwerveDrive/WheelTypes/SwerveWheel_FN.java
@@ -20,7 +20,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 
