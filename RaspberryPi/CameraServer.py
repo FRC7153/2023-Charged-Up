730876be51ab0d0a938af8532ba8a235881a862b
FRC7153 <frc7153eagles@gmail.com>
2023-02-07 20:06:17 -0500

    CameraServer added

---
+++ RaspberryPi/CameraServer.py
@@ -0,0 +1,38 @@
+from flask import Flask, Response, Markup
+import threading
+import cv2
+
+class CameraServer:
+	def __init__(self, name, port):
+		self.name = name
+		self.__app = Flask(name)
+		self.__thread = None
+		self.__port = port
+
+		self.frame = None # will be overwritten
+
+	def __serveFrame(self):
+		while True:
+			if type(self.frame).__name__ == "NoneType":
+				return Markup("<b>No frames yet</b> (refresh the page)")
+
+			_, buffer = cv2.imencode(".jpg", self.frame)
+			buffer = buffer.tobytes()
+			yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + buffer + b'\r\n')
+
+	def __videoEndpoint(self):
+		return Response(self.__serveFrame(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+	def run(self):
+		self.__app.add_url_rule("/", None, self.__videoEndpoint)
+
+		self.__thread = threading.Thread(
+			target = lambda: self.__app.run(
+				host="0.0.0.0",
+				port=self.__port,
+				debug=False,
+				use_reloader=False
+			)
+		)
+
+		self.__thread.start()
--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -3,12 +3,21 @@ import time
 import cv2
 import FRCCAN as can
 from Logger import Logger
+from CameraServer import CameraServer
 from ConePipeline import ConePipeline
 
 ## INIT
 log = Logger("logs")
 bus = can.initCAN(log)
 device = can.Device(bus, 19, log)
+
+servers = [
+	CameraServer("resize", 5000),
+	CameraServer("hsl", 5001),
+	CameraServer("erode", 5002),
+	CameraServer("dilate", 5003)
+]
+
 ConeDetector = ConePipeline()
 
 camera1 = cv2.VideoCapture(0)
@@ -16,16 +25,28 @@ camera1 = cv2.VideoCapture(0)
 mode = 1 # 1 = running, 0 = paused, -1 = shutdown
 
 ## RUN
+servers[0].run()
+servers[1].run()
+servers[2].run()
+servers[3].run()
+
 while mode != -1:
     if mode == 0: # paused
         time.sleep(0.1)
         continue
     
     success, frame = camera1.read()
+    #server1.frame = frame
 
     if not success:
         log.warn("Could not get camera frame")
         continue
 
     cones = ConeDetector.process(frame)
-    print(cones)
+
+    servers[0].frame = ConeDetector.cv_resize_output
+    servers[1].frame = ConeDetector.hsl_threshold_output
+    servers[2].frame = ConeDetector.cv_erode_output
+    servers[3].frame = ConeDetector.cv_dilate_output
+    #print(cones)
+
