19cfe8a922790f5763d0fcf10a7d3523fa40d0aa
braykoff <99614905+Braykoff@users.noreply.github.com>
2023-02-20 21:46:35 -0500

    Renamed 'swervedrive'

---
+++ src/main/java/com/frc7153/swervedrive/SwerveBase.java
@@ -0,0 +1,285 @@
+package com.frc7153.swervedrive;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.frc7153.swervedrive.wheeltypes.SwerveWheel;
+
+public class SwerveBase extends SubsystemBase {
+    // Wheels
+    private SwerveWheel fl, fr, rl, rr;
+    
+    // Kinematics & Odometry
+    private SwerveDriveKinematics kinematics;
+
+    private SwerveDriveOdometry odometry;
+    private Pose2d odometryPosition;
+
+    // State
+    private boolean periodicRunning = true;
+
+    // Max Speeds
+    private double maxDriveSpeed = 4.0;
+    private double maxSpinSpeed = 360.0;
+
+    // Get Module Position
+    private SwerveModulePosition[] getSwerveModulePositions() {
+        return new SwerveModulePosition[] {
+                fl.getState(),
+                fr.getState(),
+                rl.getState(),
+                rr.getState()
+        };
+    }
+    
+    /**
+     * Creates a new SwerveBase, with four SwerveWheels.
+     * @param frontLeft
+     * @param frontRight
+     * @param rearLeft
+     * @param rearRight
+     */
+    public SwerveBase(SwerveWheel frontLeft, SwerveWheel frontRight, SwerveWheel rearLeft, SwerveWheel rearRight, double gyroAngle) {
+        fl = frontLeft;
+        fr = frontRight;
+        rl = rearLeft;
+        rr = rearRight;
+
+        kinematics = new SwerveDriveKinematics(fl.getPosition(), fr.getPosition(), rl.getPosition(), rr.getPosition());
+
+        startOdometry(gyroAngle, 0.0, 0.0);
+    }
+
+    /**
+     * Starts (or restarts) odometry
+     * @param gyroAngle The angle of the gyroscope (degrees)
+     * @param startX Starting X position (meters)
+     * @param startY Starting Y position (meters)
+     */
+    public void startOdometry(double gyroAngle, double startX, double startY) {
+        odometry = new SwerveDriveOdometry(
+            kinematics, 
+            Rotation2d.fromDegrees(gyroAngle), 
+            getSwerveModulePositions(),
+            new Pose2d(startX, startY, Rotation2d.fromDegrees(0.0))
+        );
+    }
+
+    /**
+     * Sets the max speed for all the wheels. See the specific classes for units.
+     * @param driveSpeed Max speed of driving (Meters/Second)
+     * @param spinSpeed Max speed of spin (Degrees/Second)
+     */
+    public void setMaxSpeed(double driveSpeed, double spinSpeed) {
+        maxDriveSpeed = driveSpeed;
+        maxSpinSpeed = spinSpeed;
+    }
+
+    /**
+     * @return The max drive speed (meters/second)
+     */
+    public double getMaxDriveSpeed() { return maxDriveSpeed; }
+
+    /**
+     * @return The max spin speed (degrees/second)
+     */
+    public double getMaxSpinSpeed() { return maxSpinSpeed; }
+
+    /**
+     * Scale down speeds and distribute them to the wheels
+     * @param y
+     * @param x
+     * @param r
+     */
+    private void scaleAndDistribute(SwerveModuleState[] states) {
+        //SwerveDriveKinematics.desaturateWheelSpeeds(states, maxDriveSpeed);
+
+        fl.set(states[0]);
+        fr.set(states[1]);
+        rl.set(states[2]);
+        rr.set(states[3]);
+        periodicRunning = true;
+    }
+
+    /**
+     * Drives the robot
+     * @param y forward/backward speed (meters per second)
+     * @param x left/right speed (meters per second)
+     * @param r rotation speed (degrees per second)
+     */
+    public void driveAbsolute(double y, double x, double r) {
+        ChassisSpeeds speed = new ChassisSpeeds(y, x, -Units.degreesToRadians(r));
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed, new Translation2d(0, 0));
+
+        scaleAndDistribute(states);
+    }
+
+    /**
+     * Drives the robot, using a percentage for speed
+     * @param y forward/backward speed (percentage of max drive speed)
+     * @param x left/right speed (percentage of max drive speed)
+     * @param r rotation speed (percentage of max spin speed)
+     */
+    public void drive(double y, double x, double r) { driveAbsolute(y*maxDriveSpeed, x*maxDriveSpeed, r*maxSpinSpeed); }
+
+    /**
+     * Drives the robot, with field-oriented drive
+     * @param y forward/backward speed (meters per second)
+     * @param x left/right speed (meters per second)
+     * @param r rotation speed (degrees per second)
+     * @param deg current angle of the robot (degrees)
+     */
+    public void driveFieldOrientedAbsolute(double y, double x, double r, double deg) {
+        ChassisSpeeds speed = ChassisSpeeds.fromFieldRelativeSpeeds(
+            y, x, 
+            Units.degreesToRadians(r), 
+            Rotation2d.fromDegrees(deg)
+        );
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed);
+
+        scaleAndDistribute(states);
+    }
+
+    /**
+     * Drives the robot, with field-oriented drive, using a percentage for speed
+     * @param y forward/backward speed (percentage of max drive speed)
+     * @param x left/right speed (percentage of max drive speed)
+     * @param r rotation (percentage of max spin speed)
+     * @param deg current angle of the robot (degrees)
+     */
+    public void driveFieldOriented(double y, double x, double r, double deg) { driveFieldOrientedAbsolute(y*maxDriveSpeed, x*maxDriveSpeed, r*maxSpinSpeed, deg); }
+
+    /**
+     * Orbits the robot around a point. There is no non-absolute implementation of this yet.
+     * @param rotation The speed, in degrees per second
+     * @param centerX The X position of the center of rotation, relative to the robot's base (meters)
+     * @param centerY The Y position of the center of rotation, relative to the robot's base (meters)
+     */
+    public void orbitAbsolute(double rotation, double centerX, double centerY) {
+        ChassisSpeeds speed = new ChassisSpeeds(0.0, 0.0, -Units.degreesToRadians(rotation));
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed, new Translation2d(centerX, centerY));
+
+        scaleAndDistribute(states);
+    }
+
+    /**
+     * Drives the robot like its a tank drive base
+     * @param left The speed of the left side of the robot (meters/second)
+     * @param right The speed of the right side of the robot (meters/second)
+     */
+    public void tankDriveAbsolute(double left, double right) {
+        fl.set(0.0, SwerveMathUtils.symmetricClamp(left, maxDriveSpeed));
+        rl.set(0.0, SwerveMathUtils.symmetricClamp(left, maxDriveSpeed));
+        fr.set(0.0, SwerveMathUtils.symmetricClamp(right, maxDriveSpeed));
+        rr.set(0.0, SwerveMathUtils.symmetricClamp(right, maxDriveSpeed));
+
+        periodicRunning = true;
+    }
+
+    /**
+     * Drives the robot like its a tank drive base, using percentages for speed
+     * @param left The speed of the left side of the robot (percentage of max drive speed)
+     * @param right The speed of the right side of the robot (percentage of max drive speed)
+     */
+    public void tankDrive(double left, double right) { tankDriveAbsolute(left*maxDriveSpeed, right*maxDriveSpeed); }
+
+    /**
+     * Sets all the motors to a specific angle (for testing usually).
+     * @param angle in degrees
+     */
+    public void setAngle(double angle) {
+        fl.setAngle(angle);
+        fr.setAngle(angle);
+        rl.setAngle(angle);
+        rr.setAngle(angle);
+    }
+
+    /**
+     * Sets all the motors to a specific speed (for testing usually).
+     * @param speed
+     */
+    public void setSpeed(double speed) {
+        fl.setSpeed(speed);
+        fr.setSpeed(speed);
+        rl.setSpeed(speed);
+        rr.setSpeed(speed);
+    }
+    
+    /**
+     * Stops the robot
+     * @param reset Whether the wheels should return to a forward position
+     */
+    public void stop(boolean reset) {
+        if (reset) {
+            fl.set(0.0, 0.0);
+            fr.set(0.0, 0.0);
+            fr.set(0.0, 0.0);
+            rr.set(0.0, 0.0);
+        } else {
+            fl.setSpeed(0.0);
+            fr.setSpeed(0.0);
+            fr.setSpeed(0.0);
+            rr.setSpeed(0.0);
+        }
+    }
+
+    /**
+     * Stops the periodic methods.
+     * Periodic methods will resume as soon as one of the drive methods are called again.
+     * @param reset Whether the motor speeds should be set to 0 (if false, the robot may keep moving)
+     */
+    public void stopPeriodic(boolean reset) {
+        if (reset) { stop(false); periodic(); }
+        periodicRunning = false;
+    }
+
+    /**
+     * @param coast Whether the wheels should coast or not (brake)
+     * @param freeze Whether the wheels should stop moving
+     */
+    public void toggleCoastMode(boolean coast, boolean freeze) {
+        if (freeze) { stop(false); }
+        
+        fl.toggleCoastMode(coast);
+        fr.toggleCoastMode(coast);
+        rl.toggleCoastMode(coast);
+        rr.toggleCoastMode(coast);
+    }
+
+    /**
+     * Gets the position, according to odometry (wheel states)
+     * @return The position
+     */
+    public Pose2d getOdometricPosition() {
+        if (odometryPosition == null) {
+            DriverStation.reportWarning("Odometric position is null!", false);
+            return new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
+        }
+        return odometryPosition;
+    }
+
+    // Periodic
+    public void periodic(double gyroAngle) {
+        if (!DriverStation.isDisabled() && periodicRunning) {
+            fl.periodic();
+            fr.periodic();
+            rl.periodic();
+            rr.periodic();
+        }
+        odometryPosition = odometry.update(
+            Rotation2d.fromDegrees(gyroAngle), 
+            getSwerveModulePositions()
+        );
+    }
+
+    @Override
+    public void periodic() { periodic(0.0); }
+}
---
+++ src/main/java/com/frc7153/swervedrive/wheeltypes/SwerveWheel_FN.java
@@ -0,0 +1,201 @@
+package com.frc7153.swervedrive.wheeltypes;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController.AccelStrategy;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import com.frc7153.swervedrive.SwerveBase;
+import com.frc7153.swervedrive.SwerveMathUtils;
+
+/**
+ * Swerve Wheel that uses a Falcon500 for the drive motor and Neo Brushless (with CAN Spark Max) for spin motor.
+ * Uses CANCoder absolute encoder for absolute position. <br><br>
+ * <b>First used in 2023 season (Charged Up)</b>
+ */
+public class SwerveWheel_FN implements SwerveWheel {
+    // CONFIG VALUES //
+    private static double k_SPIN_RATIO = 150.0 / 7.0;
+    private static double k_DRIVE_RATIO = 6.12; // For standard speed module
+    private static double k_WHEEL_CIRCUMFERENCE = Units.inchesToMeters(4.0) * Math.PI; // 3.75
+
+    private static int k_SPIN_CURRENT_LIMIT = 40;
+    private static int k_DRIVE_CURRENT_LIMIT = 20;
+    private static int k_DRIVE_CURRENT_PEAK = 25;
+    private static double k_DRIVE_CURRENT_PEAK_DURATION = 0.1;
+    private static double k_DRIVE_ERR = 0.5;
+    private static double k_DRIVE_DEADBAND = 0.5; // Meters ber second
+
+    private static int k_SPIN_PID_INDEX = 0;
+    private static int k_DRIVE_PID_INDEX = 0;
+
+    private static double spin_kP = 0.3;
+    private static double spin_kI = 0.00001;
+    private static double spin_kD = 0.0;
+    private static double spin_kO = 10.0;
+
+    private static double drive_kP = 0.05;
+    private static double drive_kI = 0.0;
+    private static double drive_kD = 0.0;
+    private static double drive_kF = 0.0;
+
+    private static double k_SPIN_MAX_ACCEL = 0.8;
+    
+    // Motors, Encoders, PID
+    private TalonFX driveWheel;
+    private CANSparkMax spinWheel;
+
+    private RelativeEncoder spinRelEncoder;
+    private SparkMaxPIDController spinPID;
+
+    // Position
+    private Translation2d pos;
+
+    @Override
+    public Translation2d getPosition() {return pos; }
+
+    /**
+     * Creates a new Swerve Wheel. Expects a Falcon500 (TalonFX) for the drive wheel and a Rev Brushless NEO for spin motor.
+     * Both should communicate over CAN bus.
+     * 
+     * @param drive The CAN id of the drive wheel (Falcon500)
+     * @param spin The CAN id of the spin wheel (Rev Brushless NEO)
+     * @param canCoder The CAN id of the CANCoder
+     * @param x The x position of the wheel, relative to the center of the base, in meters
+     * @param y The y position of the wheel, relative to the center of the base, in meters
+     * @param spinHomeLocation When the wheel's direction is 0 degrees (forward), what is the output of the ABSOLUTE encoder (degrees)?
+     */
+    public SwerveWheel_FN(int spin, int drive, int canCoder, double x, double y, double spinHomeLocation) {
+        // Declare and Configure Motors
+        driveWheel = new TalonFX(drive);
+        spinWheel = new CANSparkMax(spin, MotorType.kBrushless);
+        
+        toggleCoastMode(false);
+
+        spinWheel.setSmartCurrentLimit(k_SPIN_CURRENT_LIMIT);
+        driveWheel.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, k_DRIVE_CURRENT_LIMIT, k_DRIVE_CURRENT_PEAK, k_DRIVE_CURRENT_PEAK_DURATION));
+        driveWheel.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, k_DRIVE_CURRENT_LIMIT, k_DRIVE_CURRENT_PEAK, k_DRIVE_CURRENT_PEAK_DURATION));
+
+        spinWheel.setInverted(true);
+
+        // Declare and Configure Encoders
+        CANCoder spinAbsEncoder = new CANCoder(canCoder);
+        spinRelEncoder = spinWheel.getEncoder();
+        
+        spinAbsEncoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+
+        // Set Relative Encoder Offset
+        spinRelEncoder.setPosition((spinAbsEncoder.getAbsolutePosition() - spinHomeLocation) * k_SPIN_RATIO / 360.0);
+
+        // Spin PID
+        spinPID = spinWheel.getPIDController();
+
+        if (spinPID.getP(k_SPIN_PID_INDEX) != spin_kP || spinPID.getI(k_SPIN_PID_INDEX) != spin_kI || spinPID.getD(k_SPIN_PID_INDEX) != spin_kD|| spinPID.getOutputMax(k_SPIN_PID_INDEX) != spin_kO || spinPID.getOutputMin(k_SPIN_PID_INDEX) != -spin_kO) {
+            spinPID.setP(spin_kP, k_SPIN_PID_INDEX);
+            spinPID.setI(spin_kI, k_SPIN_PID_INDEX);
+            spinPID.setD(spin_kD, k_SPIN_PID_INDEX);
+            spinPID.setOutputRange(-spin_kO, spin_kO, k_SPIN_PID_INDEX);
+            spinWheel.burnFlash();
+            DriverStation.reportWarning(String.format("Had to re-write Swerve PID values for CAN Spark Max %s", spin), false);
+        }
+
+        // Drive PID
+        driveWheel.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+        driveWheel.configAllowableClosedloopError(k_DRIVE_PID_INDEX, k_DRIVE_ERR);
+        
+        driveWheel.config_kP(k_DRIVE_PID_INDEX, drive_kP);
+        driveWheel.config_kI(k_DRIVE_PID_INDEX, drive_kI);
+        driveWheel.config_kD(k_DRIVE_PID_INDEX, drive_kD);
+        driveWheel.config_kF(k_DRIVE_PID_INDEX, drive_kF);
+
+        /*
+         * The X and Y values are implemented in WPI's library oddly:
+         * "Positive x values represent moving toward the front of the robot whereas positive 
+         * y values represent moving toward the left of the robot."
+         * It is changed below to make it easier to understand.
+         * 
+         * See https://docs.wpilib.org/en/stable/docs/software/kinematics-and-odometry/swerve-drive-kinematics.html 
+         */
+        pos = new Translation2d(y, -x);
+    }
+
+    /**
+     * Enables/configs trapezoidal motion trajectory
+     * @param base The robot base (used to get max drive speed and spin speed)
+     */
+    public void enableMotionAccelerationStrategy(SwerveBase base) {
+        spinPID.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal, k_SPIN_PID_INDEX);
+        spinPID.setSmartMotionMaxAccel(k_SPIN_MAX_ACCEL, k_SPIN_PID_INDEX);
+        spinPID.setSmartMotionMaxVelocity(base.getMaxDriveSpeed(), k_SPIN_PID_INDEX);
+        spinPID.setSmartMotionMinOutputVelocity(0.05, k_SPIN_PID_INDEX);
+    }
+
+    // Get Angle from Relative Encoder (degrees)
+    private double getAngleFromRelative() {
+        return SwerveMathUtils.normalizeAngle180(spinRelEncoder.getPosition() * 360.0 / k_SPIN_RATIO);
+    }
+
+    // Coast
+    @Override
+    public void toggleCoastMode(boolean coast) {
+        spinWheel.setIdleMode((coast) ? IdleMode.kCoast : IdleMode.kBrake);
+        driveWheel.setNeutralMode((coast) ? NeutralMode.Coast : NeutralMode.Brake);
+    }
+
+    // Get State
+    @Override
+    public SwerveModulePosition getState() {
+        return new SwerveModulePosition(
+            SwerveMathUtils.falcon500PositionToRotations(driveWheel.getSelectedSensorPosition()) / k_DRIVE_RATIO * k_WHEEL_CIRCUMFERENCE,
+            Rotation2d.fromDegrees(SwerveMathUtils.normalizeAngle360(getAngleFromRelative()))
+        );
+    }
+
+    // Set State
+    @Override
+    public void setAngle(double angle) {
+        angle = SwerveMathUtils.normalizeAngle180(angle); // Normalize -180 to 180
+        angle = (angle / 360.0 * k_SPIN_RATIO); // Convert to NEO position
+        angle = SwerveMathUtils.calculateContinuousMovement(spinRelEncoder.getPosition(), angle, k_SPIN_RATIO); // Find quickest route
+        spinPID.setReference(angle, ControlType.kPosition, k_SPIN_PID_INDEX); // Set PID setpoint
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        speed = SwerveMathUtils.applyDeadband(speed, k_DRIVE_DEADBAND); // Apply deadband
+        double velocity = (speed / k_WHEEL_CIRCUMFERENCE) * 60.0; // Convert to rotations per minute
+        velocity *= k_DRIVE_RATIO; // Convert to Falcon500 position
+        driveWheel.set(ControlMode.Velocity, SwerveMathUtils.rpmToFalcon500Velocity(velocity)); // Set set point, in Falcon500 encoder's velocity
+    }
+
+    @Override
+    public void set(SwerveModuleState state) {
+        state = SwerveModuleState.optimize(
+            state, 
+            Rotation2d.fromDegrees(SwerveMathUtils.normalizeAngle360(getAngleFromRelative()))
+        );
+        set(state.angle.getDegrees(), state.speedMetersPerSecond);
+    }
+
+    // Periodic
+    @Override
+    public void periodic() {}
+}
